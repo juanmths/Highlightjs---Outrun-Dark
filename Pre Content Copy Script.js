5bfcedbb9232432cbce2f52265a118fb95b306a2
Juan Matheus <70798339+juanmths@users.noreply.github.com>
2022-07-23 22:32:23 +0700

    Pre Content Copy Script.js

---
+++ Pre Content Copy Script.js
@@ -0,0 +1,9 @@
+<!-- Pre Content Copy Script By Juan Mths-->
+        for (var preClick = document.getElementsByTagName("pre"), i = 0; i < preClick.length; i++) preClick[i]
+            .addEventListener("dblclick", function() {
+                var e = getSelection(),
+                    o = document.createRange();
+                o.selectNodeContents(this), e.removeAllRanges(), e.addRange(o), document.execCommand("copy"), e
+                    .removeAllRanges(), document.querySelector("#toastNotif").innerHTML =
+                    "<span>Copied to clipboard!</span>"
+            }, !1);
